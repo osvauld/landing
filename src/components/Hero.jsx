b376cd6b028086acf8d7b431a136f5363481794f
tony <tonyantony300@gmail.com>
2024-02-16 18:03:00 +0530

    navbar in progress

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
 
   return (
     <section
-      className="w-screen  flex justify-center items-center bg-customDarkBg1 mb-[28vw] md:mb-[18vw] lg:mb-[10vw] xl:mb-[13vw] 2xl:mb-60 hero-bg-gradient pb-24 sm:pb-32 md:pb-44 lg:pb-0"
+      className="w-screen  flex justify-center items-center bg-dark1 mb-[28vw] md:mb-[18vw] lg:mb-[10vw] xl:mb-[13vw] 2xl:mb-60 hero-bg-gradient pb-24 sm:pb-32 md:pb-44 lg:pb-0"
       id="home"
     >
       <div className="w-full md:w-[800px] xl:w-[900px] flex flex-col justify-center items-center pt-16 md:pt-16 lg:pt-20 text-center">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,18 @@ import { OsvauldLogo } from "../assets/logos/OsvauldLogo";
 import { GithubIcon } from "../assets/icons/GithubIcon";
 
 const navbarLinks = [
-  { label: "Home", href: "#home", ariaLabel: "Home" },
-  { label: "Features", href: "#features", ariaLabel: "Features" },
-  { label: "Feedback", href: "#feedback", ariaLabel: "Feedback" },
-  { label: "FAQ", href: "#FAQ", ariaLabel: "FAQ" },
+  { label: "Why osvauld", href: "#why", ariaLabel: "why osvauld" },
+  { label: "Community", href: "#community", ariaLabel: "Community" },
+  { label: "About", href: "#about", ariaLabel: "About" },
   { label: "Documentation", href: "#home", ariaLabel: "Documentation" },
+  { label: "Get started", href: "#getStarted", ariaLabel: "Get started" },
 ];
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="w-full h-20 flex flex-col justify-center items-center fixed bg-customDarkBg1 lg:bg-customDarkBgTransparent z-40 lg:backdrop-blur-xl">
+    <nav className="w-full h-20 flex flex-col justify-center items-center fixed bg-dark1 lg:bg-dark1Transparent z-40 lg:backdrop-blur-xl">
       <div className="2xl:w-[1280px] xl:w-10/12 w-11/12 flex justify-between items-center relative">
         <motion.div
           initial={{ opacity: 0 }}
@@ -29,8 +29,8 @@ export const Navbar = () => {
               <div className="text-white mr-2 text-6xl">
                 <OsvauldLogo />
               </div>
-              <div className="text-white font-['Inter'] font-bold text-xl">
-                <span className='font-normal'>os</span>vauld
+              <div className="text-blue1 font-['Inter'] font-bold text-xl">
+                osvauld
               </div>
             </div>
           </a>
@@ -60,21 +60,34 @@ export const Navbar = () => {
           transition={{ duration: 0.3 }}
           exit={{ opacity: 0 }}
         >
+          <div className='flex justify-center items-center gap-1'>
           <div className="grow basis-0 justify-end hidden lg:flex">
-            <a
-              className="text-white custom-border-gray rounded-xl
-           bg-customDarkBg2 hover:bg-customDarkBg3  border-gray-700 pl-6 pr-8 pt-2 pb-2 text-sm flex"
-              href="https://github.com/osvauld"
-              target="_blank"
-              aria-label="source code"
-            >
-              <GithubIcon />
-              <span className="pt-px">Source code</span>
-            </a>
+              <a
+                className="text-white rounded-lg
+            bg-dark1 cursor-pointer flex whitespace-nowrap text-sm  border border-blue1  px-2.5 py-1.5 "
+                href="https://github.com/osvauld"
+                target="_blank"
+                aria-label="book demo"
+              >
+                <span className="pt-px">Book a demo</span>
+              </a>
+            </div>
+              <div className="grow basis-0 justify-end hidden lg:flex">
+              <a
+                className="text-dark1
+            bg-blue1 cursor-pointer rounded-lg px-2.5 py-1.5 text-sm whitespace-nowrap  flex"
+                href="https://github.com/osvauld"
+                target="_blank"
+                aria-label="get started"
+              >
+                <span className="pt-px">Get started</span>
+              </a>
+            </div>
+          
           </div>
         </motion.div>
         <div
-          className="lg:hidden flex flex-col  px-2 py-3 border-solid border border-gray-600 rounded-md cursor-pointer hover:bg-customDarkBg2"
+          className="lg:hidden flex flex-col  px-2 py-3  bg-dark1 border-solid border border-gray-600 rounded-md cursor-pointer"
           onClick={() => setIsOpen(!isOpen)}
         >
           <div className="w-5 h-0.5 bg-gray-500  mb-1"></div>
@@ -92,8 +105,8 @@ export const Navbar = () => {
             exit={{ opacity: 0 }}
           >
             <div
-              className="flex flex-col mt-16 lg:hidden absolute top-4 left-0  bg-customDarkBg1 z-50 w-full 
-        items-center gap-10 pb-10 border-y border-solid border-customDarkBg3 pt-10
+              className="flex flex-col mt-16 lg:hidden absolute top-4 left-0  bg-dark1 z-50 w-full 
+        items-center gap-10 pb-10 border-y border-solid border-customDarkBg3 pt-10 text-sm
         "
             >
               {navbarLinks.map(({ label, href, ariaLabel }) => (
@@ -108,9 +121,9 @@ export const Navbar = () => {
                 </a>
               ))}
               <a
-                className="text-white custom-border-gray rounded-xl
-           bg-customDarkBg2 hover:bg-customDarkBg3  border-gray-700 pl-6 pr-8 pt-2 pb-2 text-sm flex"
-                href="https://github.com/matt765/Tidestream"
+                className="text-dark1 custom-border-gray rounded-xl
+           bg-blue1 cursor-pointer pl-6 pr-8 pt-2 pb-2 text-sm flex justify-center items-center whitespace-nowrap"
+                href="https://github.com/"
                 target="_blank"
               >
                 <GithubIcon />
