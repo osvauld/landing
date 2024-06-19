ddf9810914a4126b75c5329b78ea7234ed15943c
tony <tonyantony300@gmail.com>
2024-06-19 13:02:24 +0530

    manual sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://osvauld.com",
-  integrations: [react(), tailwind(), sitemap()]
+  integrations: [react(), tailwind()],
 });
