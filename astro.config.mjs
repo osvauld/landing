2d585eec2dc3742248815634946b779f1e4feb05
tony <tonyantony300@gmail.com>
2024-06-21 19:41:05 +0530

    npm to yarn change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://osvauld.com",
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind(),
+    starlight({
+      title: "Docs at a subpath",
+    }),
+  ],
 });
