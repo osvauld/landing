c918624cdd72805b0ff077d4c4e92ff23f70135b
tony <tonyantony300@gmail.com>
2024-07-13 15:47:32 +0530

    todays styles

--- src/components/additional/PricingCompoent.jsx
+++ src/components/additional/PricingCompoent.jsx
@@ -9,14 +9,14 @@ export const PricingCompoent = () => {
       transition={{ duration: 0.2 }}
       className="w-full flex-grow flex flex-col items-center max-w-[1200px] mx-auto bg-faqDark px-4 pt-28 text-white"
     >
-      <span className="text-[44px] flex flex-col items-center md:text-[56px] gradient-text-bottom font-semibold ">
+      <span className="text-[32px] md:text-[36px] flex flex-col items-center lg:text-[56px] gradient-text-bottom font-semibold mb-20">
         <h1>Affordable Pricing</h1>
         <h1>for teams of all sizes</h1>
       </span>
-      <div className="cards grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 gap-6">
-        <div className="border border-red-500 h-[300px] min-w-[200px]"></div>
-        <div className="border border-white1 h-[300px]"></div>
-        <div className="border border-yellow-300 h-[300px] md:col-span-2"></div>
+      <div className="cards grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 gap-6 md:gap-x-10 w-full min-w-[300px] max-w-[331px] md:max-w-[800px] ">
+        <div className="pricing-card h-[552px] w-full "></div>
+        <div className="pricing-card h-[552px] w-full"></div>
+        <div className="sm:pricing-card border border-faqBorder rounded-xl h-[552px] w-full md:h-[452px] md:col-span-2"></div>
       </div>
     </motion.section>
   );
