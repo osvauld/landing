6c4b0426ebbbb064b2f48b6adbd8e675e029231d
tony <tonyantony300@gmail.com>
2024-07-19 15:33:18 +0530

    refactoring code and config in progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import starlight from "@astrojs/starlight";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://osvauld.com",
-  integrations: [
-    react(),
-    tailwind(),
-    starlight({
-      title: "Docs at a subpath",
-    }),
-  ],
+  integrations: [react(), tailwind(), starlight({
+    title: "Docs at a subpath"
+  }), sitemap()]
 });
