d12638b8c2025d48518749b79c737e82c4997e3c
tony <tonyantony300@gmail.com>
2024-07-30 21:29:00 +0530

    react version bumb down

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import starlight from "@astrojs/starlight";
 import sitemap from "@astrojs/sitemap";
 
 import playformCompress from "@playform/compress";
@@ -9,7 +8,5 @@ import playformCompress from "@playform/compress";
 // https://astro.build/config
 export default defineConfig({
   site: "https://osvauld.com",
-  integrations: [react(), tailwind(), starlight({
-    title: "Docs at a subpath"
-  }), sitemap(), playformCompress()]
+  integrations: [react(), tailwind(), sitemap(), playformCompress()],
 });
