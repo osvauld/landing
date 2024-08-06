171aae594d279abd4b237bdc6f9c852e2614cb56
tony <tonyantony300@gmail.com>
2024-08-06 14:39:45 +0530

    compression functinality removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,8 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-import playformCompress from "@playform/compress";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://osvauld.com",
-  integrations: [react(), tailwind(), sitemap(), playformCompress()],
+  integrations: [react(), tailwind(), sitemap()],
 });
