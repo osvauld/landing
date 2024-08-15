2f4a0f556efd9940d1685b95a9df9451937504b5
tony <tonyantony300@gmail.com>
2024-08-15 15:42:23 +0530

    opensource section modded

--- src/components/OpenSourceV2.jsx
+++ src/components/OpenSourceV2.jsx
@@ -9,21 +9,23 @@ export const OpenSourceV2 = () => (
     whileInView={{ opacity: 1 }}
     viewport={{ once: true }}
     transition={{ duration: 0.5, delay: 0.2 }}
-    className="h-[56rem] xs:h-[42rem] w-full max-w-[1200px] mx-auto px-8"
+    className="h-[56rem] xs:h-[32rem] w-full max-w-[1200px] flex flex-col justify-start gap-4 md:flex-row md:justify-around  mx-auto mt-0 lg:mt-20 px-8"
   >
-    <h2 className="text-5xl lg:text-6xl  font-light font-Jakartha text-center lg:text-left gradient-text-bottom  leading-tight lg:leading-[1.15] mb-6">
-      Audit, contribute, <br /> and redistribute. <br />
-      <span className="font-semibold">
-        Osvauld is fully <br /> open-source.
-      </span>{" "}
-    </h2>
-    <h3 className=" text-center lg:text-left font-Jakartha font-extralight text-white1 gradient-text-bottom text-4xl ">
-      Under AGPL-3.0 license.
-    </h3>
+    <div>
+      <h2 className="text-5xl lg:text-6xl  font-light font-Jakartha text-center lg:text-left gradient-text-bottom  leading-tight lg:leading-[1.15] mb-6">
+        Audit, contribute, <br /> and redistribute. <br />
+        <span className="font-semibold">
+          Osvauld is fully <br /> open-source.
+        </span>{" "}
+      </h2>
+      <h3 className=" text-center lg:text-left font-Jakartha font-extralight text-white1 gradient-text-bottom text-4xl ">
+        Under AGPL-3.0 license.
+      </h3>
+    </div>
 
-    <div className="w-full h-[15rem] grid grid-rows-2 grid-cols-2 md:grid-rows-1 md:grid-cols-4 gap-4 mt-12 text-base lg:text-xl">
+    <div className="grid grid-cols-2 grid-rows-2 gap-4 h-[24rem]  text-base lg:text-xl">
       <a
-        className="bg-dark5 max-w-[16rem] max-h-[10rem] rounded-md flex flex-col justify-center items-center text-faqAnswer gap-2 lg:gap-4 cursor-pointer"
+        className="bg-dark5 h-[12rem] rounded-md flex flex-col justify-center items-center text-faqAnswer  cursor-pointer col-span-2  px-2"
         target="_blank"
         href="https://github.com/osvauld/osvauld"
       >
@@ -31,7 +33,7 @@ export const OpenSourceV2 = () => (
         <span>Star us on Github</span>
       </a>
       <a
-        className="bg-dark5 max-w-[16rem] max-h-[10rem] rounded-md flex flex-col justify-center items-center text-faqAnswer gap-2 lg:gap-4 cursor-pointer"
+        className="bg-dark5 h-[12rem] rounded-md flex flex-col justify-center items-center text-faqAnswer  cursor-pointer col-span-1 px-2"
         target="_blank"
         href="https://github.com/osvauld/osvauld/issues"
       >
@@ -39,7 +41,7 @@ export const OpenSourceV2 = () => (
         <span className="whitespace-nowrap">Become a Contributor</span>
       </a>
       <a
-        className="bg-dark5 max-w-[16rem] max-h-[10rem] rounded-md flex flex-col justify-center items-center text-faqAnswer gap-2 lg:gap-4 cursor-pointer"
+        className="bg-dark5 h-[12rem] rounded-md flex flex-col justify-center items-center text-faqAnswer  cursor-pointer col-span-1  px-2"
         target="_blank"
         href="https://buymeacoffee.com/osvauld"
       >
