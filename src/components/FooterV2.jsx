84f5bb5a0a64dfcdb2803ac633438d980a07a7e7
tony <tonyantony300@gmail.com>
2024-08-19 19:19:02 +0530

    footer v2 second commit

--- src/components/FooterV2.jsx
+++ src/components/FooterV2.jsx
@@ -1,11 +1,54 @@
+import { DiscordIcon } from "../assets/icons/DiscordIcon";
+import { LinktreeIcon } from "../assets/icons/LinktreeIcon";
+import { LinkdinIcon } from "../assets/icons/LinkdinIcon";
+import { InstagramIcon } from "../assets/icons/InstagramIcon";
+import { GithubIcon } from "../assets/icons/GithubIcon";
 import { FooterOsvauld } from "../assets/logos/FooterOsvauld";
 
 export const Footer = () => {
   return (
     <section className="w-full bg-blue1">
-      <footer className="relative mx-auto h-[40rem] px-[24px] pt-[32px]  min-h-[749px] max-w-[1312px]  lg:min-h-[545px] lg:px-[60px] lg:pt-[60px]">
+      <footer className="relative mx-auto h-[46rem] px-[24px] pt-[32px]  max-w-[1312px]  min-h-[545px] lg:px-[60px] lg:pt-[60px]">
         <div className="flex h-full flex-col gap-32">
-          <div className="flex flex-col justify-between lg:flex-row"></div>
+          <div className="flex flex-col gap-14">
+            <div className="icons flex items-center gap-10 mx-auto">
+              <DiscordIcon width={"45px"} height={"45px"} />{" "}
+              <LinktreeIcon color={"black"} size={"45px"} />
+              <LinkdinIcon color={"black"} size={"45px"} />
+              <InstagramIcon color={"black"} size={"45px"} />
+              <GithubIcon color={"black"} size={"45px"} />
+            </div>
+            <h6 className="mx-auto text-[54px] xl:text-[64px] tracking-[-4px] font-Jakartha font-semibold">
+              Protecting what matters the most.
+            </h6>
+            <div className="flex flex-col xs:flex-row justify-center items-center gap-4">
+              <button className="px-6 py-3 bg-black rounded-lg text-blue1 text-xl font-semibold flex items-center gap-4">
+                Get Started
+                <span>
+                  <DiscordIcon color="#89B4FA" width={"45px"} height={"45px"} />
+                </span>
+              </button>
+              <button className=" px-6 py-3 bg-black rounded-lg text-blue1 text-xl font-semibold flex items-center gap-4">
+                Request a Demo
+                <span>
+                  <DiscordIcon color="#89B4FA" width={"45px"} height={"45px"} />
+                </span>
+              </button>
+            </div>
+            <div className="text-center flex flex-col gap-4">
+              <div className="flex items-center gap-4">
+                <a href="" className="ml-auto tracking-tight">
+                  Terms of Use
+                </a>
+                <a href="" className="mr-auto tracking-tight">
+                  Privacy Policy
+                </a>
+              </div>
+              <p className="font-medium">
+                Osvauld Security Solutions Pvt Limited @ 2024
+              </p>
+            </div>
+          </div>
           <div className="absolute bottom-0 left-1/2 w-full -translate-x-1/2 px-[24px] lg:px-[60px]">
             <div className="flex w-full justify-center">
               <FooterOsvauld />
