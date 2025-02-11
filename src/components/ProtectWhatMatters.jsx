e1664111e54611cc6c96853269f23cf241e2c97d
tony <tonyantony300@gmail.com>
2025-02-11 17:24:04 +0530

    horizontal to scroll

--- src/components/ProtectWhatMatters.jsx
+++ src/components/ProtectWhatMatters.jsx
@@ -10,7 +10,7 @@ export const ProtectWhatMatters = () => {
       whileInView={{ opacity: 1 }}
       viewport={{ once: true }}
       transition={{ duration: 0.2, delay: 0.2 }}
-      className="min-h-[75rem] sm:min-h-[60rem] w-full flex-grow flex flex-col items-center max-w-[1400px] mx-auto bg-dark4 px-4 pt-0 sm:pt-12 text-white relative"
+      className="min-h-[45rem] lg:min-h-[55rem] w-full flex-grow flex flex-col items-center max-w-[1400px] mx-auto bg-dark4 px-4 pt-0 sm:pt-12 text-white relative"
     >
       <div className="flex justify-center gap-20 relative">
         <div className="flex items-start min-w-[300px]">
@@ -32,13 +32,15 @@ export const ProtectWhatMatters = () => {
         </span>
         <span className="flex flex-col items-center mb-14 mt-6 text-[40px] md:text-[48px] lg:text-7xl font-Jakartha font-medium tracking-tight text-balance gradient-text-bottom py-4 leading-10 md:leading-[54px]">
           <h2 className="text-center lg:text-left ">
-            Protecting what <br /> matters the most.
+            Giving power <br /> back to people.
           </h2>
         </span>
 
         <span className="pricing-content text-sm px-4 md:text-lg mb-16 text-center">
-          Managing your digital life doesn’t have to be a headache.
-          <br /> Forget the stress of remembering countless passwords <br /> or
+          Managing your digital life <br className="block sm:hidden" /> doesn’t
+          have to be a headache.
+          <br /> Forget the stress of remembering{" "}
+          <br className="block sm:hidden" /> countless passwords <br /> or
           worrying about your data’s security.
         </span>
 
@@ -46,8 +48,8 @@ export const ProtectWhatMatters = () => {
           OSVAULD IS HERE TO:
         </span>
 
-        <div className="flex flex-wrap justify-center items-center gap-4 mt-8">
-          <SpotlightCard className=" feature-card ">
+        <div className="flex items-center gap-4 mt-8 pb-4 max-w-full overflow-x-scroll scrollbar-thin">
+          <SpotlightCard className=" feature-card shrink-0 ">
             <span className="gradient-text-bottom font-Jakartha font-semibold text-2xl tracking-tighter ">
               Keep everything safe{" "}
             </span>
@@ -57,7 +59,7 @@ export const ProtectWhatMatters = () => {
             </p>
           </SpotlightCard>
 
-          <SpotlightCard className="feature-card ">
+          <SpotlightCard className="feature-card shrink-0 ">
             <span className="gradient-text-bottom font-Jakartha font-semibold text-2xl tracking-tighter ">
               Work without internet{" "}
             </span>
@@ -66,7 +68,7 @@ export const ProtectWhatMatters = () => {
             </p>
           </SpotlightCard>
 
-          <SpotlightCard className="feature-card">
+          <SpotlightCard className="feature-card shrink-0">
             <span className="gradient-text-bottom font-Jakartha font-semibold text-2xl tracking-tighter ">
               Sync across devices{" "}
             </span>
@@ -74,7 +76,7 @@ export const ProtectWhatMatters = () => {
               Get seamless access to your <br /> data on all your devices.
             </p>
           </SpotlightCard>
-          <SpotlightCard className="feature-card">
+          <SpotlightCard className="feature-card shrink-0">
             <span className="gradient-text-bottom font-Jakartha font-semibold text-2xl tracking-tighter ">
               Put you in charge{" "}
             </span>
